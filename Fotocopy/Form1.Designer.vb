d5e72fad65c8f423e00a1c334b1d8319864a9170
FaridFK <74176005+FaridFK@users.noreply.github.com>
2022-06-22 22:25:25 +0700

    Add files via upload

---
+++ Fotocopy/Form1.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.LabelTotal = New System.Windows.Forms.Label()
+        Me.BtnTotal = New System.Windows.Forms.Button()
+        Me.ListPelanggan = New System.Windows.Forms.ListBox()
+        Me.BoxCopy = New System.Windows.Forms.TextBox()
+        Me.LabelLembar = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(127, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(200, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Fotocopy Informatic"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(86, 62)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(80, 16)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Pelanggan :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(74, 138)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(92, 16)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Jumlah Copy :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(42, 174)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(124, 16)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Harga per Lembar :"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.LabelTotal)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(23, 210)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(293, 77)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Bayar Total"
+        '
+        'LabelTotal
+        '
+        Me.LabelTotal.AutoSize = True
+        Me.LabelTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelTotal.Location = New System.Drawing.Point(22, 31)
+        Me.LabelTotal.Name = "LabelTotal"
+        Me.LabelTotal.Size = New System.Drawing.Size(39, 25)
+        Me.LabelTotal.TabIndex = 0
+        Me.LabelTotal.Text = "Rp"
+        '
+        'BtnTotal
+        '
+        Me.BtnTotal.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BtnTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnTotal.Location = New System.Drawing.Point(336, 224)
+        Me.BtnTotal.Name = "BtnTotal"
+        Me.BtnTotal.Size = New System.Drawing.Size(68, 53)
+        Me.BtnTotal.TabIndex = 5
+        Me.BtnTotal.Text = "Total"
+        Me.BtnTotal.UseVisualStyleBackColor = True
+        '
+        'ListPelanggan
+        '
+        Me.ListPelanggan.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListPelanggan.FormattingEnabled = True
+        Me.ListPelanggan.ItemHeight = 16
+        Me.ListPelanggan.Location = New System.Drawing.Point(196, 62)
+        Me.ListPelanggan.Name = "ListPelanggan"
+        Me.ListPelanggan.Size = New System.Drawing.Size(193, 52)
+        Me.ListPelanggan.TabIndex = 6
+        '
+        'BoxCopy
+        '
+        Me.BoxCopy.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BoxCopy.Location = New System.Drawing.Point(196, 132)
+        Me.BoxCopy.Name = "BoxCopy"
+        Me.BoxCopy.Size = New System.Drawing.Size(193, 26)
+        Me.BoxCopy.TabIndex = 7
+        '
+        'LabelLembar
+        '
+        Me.LabelLembar.AutoSize = True
+        Me.LabelLembar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelLembar.Location = New System.Drawing.Point(192, 171)
+        Me.LabelLembar.Name = "LabelLembar"
+        Me.LabelLembar.Size = New System.Drawing.Size(30, 20)
+        Me.LabelLembar.TabIndex = 8
+        Me.LabelLembar.Text = "Rp"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(430, 311)
+        Me.Controls.Add(Me.LabelLembar)
+        Me.Controls.Add(Me.BoxCopy)
+        Me.Controls.Add(Me.ListPelanggan)
+        Me.Controls.Add(Me.BtnTotal)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Fotocopy"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents LabelTotal As Label
+    Friend WithEvents BtnTotal As Button
+    Friend WithEvents ListPelanggan As ListBox
+    Friend WithEvents BoxCopy As TextBox
+    Friend WithEvents LabelLembar As Label
+End Class
---
+++ Fotocopy/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ListPelanggan.Items.Add("Bukan Pelanggan")
+        ListPelanggan.Items.Add("Pelanggan")
+    End Sub
+
+    Private Sub BoxCopy_TextChanged(sender As Object, e As EventArgs) Handles BoxCopy.TextChanged
+        Dim jmlCopy As Integer = Val(BoxCopy.Text)
+        Dim index As Integer = Val(ListPelanggan.SelectedIndex)
+
+        If jmlCopy >= 100 And index <> 0 Then
+            LabelLembar.Text = 65
+
+        ElseIf jmlCopy < 100 And index <> 0 Then
+            LabelLembar.Text = 70
+
+        ElseIf jmlCopy >= 100 And index = 0 Then
+            LabelLembar.Text = 80
+
+        Else
+            LabelLembar.Text = 100
+        End If
+    End Sub
+
+    Private Sub BtnTotal_Click(sender As Object, e As EventArgs) Handles BtnTotal.Click
+        Dim jmlCopy As Integer = Val(BoxCopy.Text)
+        Dim harga As Integer = Val(LabelLembar.Text)
+
+        LabelTotal.Text = jmlCopy * harga
+    End Sub
+End Class
